b0ea4188278fd31bd3d19db4d6cf5c157361bca2
yse-yoo <youhei_yoshikawa@yse-c.net>
2026-05-18 11:46:39 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [HomeController::class, 'about']);
 
 Route::get('/product/{id}', function ($id) {
     // TODO: ここで商品IDを使用して、データベースから商品情報を取得するなどの処理を行うことができます。
